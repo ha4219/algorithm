7fa5f55b7c82d9c6c4265a7de552940d4a3e578e
Replit user <ha4219@naver.com>
2021-03-24 13:44:21 +0000

    solved boj 1562

--- boj/boj 1562.py
+++ boj/boj 1562.py
@@ -1,17 +1,3 @@
----
-layout: post
-title: boj 1562 해설
-categories: [Baekjoon]
-tags: [백준 1562, boj 1562]
----
-
-# 백준 1562번
-
-## dp, Bitmasking
-
-처음에는 이중 for문으로 가능할까 생각했지만 path, 0~9까지 숫자가 모두 사용해야 함으로 dfs 함수를 사용했다. 모든 수를 거쳤을 때, path==(1<<10)-1이여야 return 1을 반납하여 개수를 측정했다.
-
-```python
 from sys import stdin
 
 
@@ -19,6 +5,7 @@ input = stdin.readline
 
 n = int(input())
 d = [[[-1] * (1<<10) for _ in range(11)] for __ in range(n+1)]
+t = [(1<<i) for i in range(10)]
 mod = 10**9
 for i in range(1,10):
     d[1][i][(1<<i)]=1
@@ -40,4 +27,3 @@ res = 0
 for i in range(1,10):
     res = (res+dfs(n,i,0))%mod
 print(res)
-```
