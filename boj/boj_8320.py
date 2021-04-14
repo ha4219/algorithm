712c7d1e1ebcc363b6a1af1fb62f3cba48b2e049
Replit user <ha4219@naver.com>
2021-04-14 13:27:35 +0000

    solved boj 8320

---
+++ boj/boj_8320.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+
+input = stdin.readline
+
+n = int(input())
+
+res = 0
+
+for i in range(1,n+1):
+    for j in range(i,n+1):
+        if i*j<=n:
+            res += 1
+print(res)
