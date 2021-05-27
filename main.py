7f77e2c31a5ef8121bd251eb8e2862bccb71480b
Replit user <ha4219@naver.com>
2021-05-27 13:17:15 +0000

    solved boj 2170

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-from sys import stdin, setrecursionlimit
-from math import *
+from sys import stdin, setrecursionlimit, maxsize
+
 
 setrecursionlimit(10**6)
-
 input = stdin.readline
 
-MAX = 10001
-p = [1] * MAX
-p[0] = 0
-p[1] = 0
+n = int(input())
+a = [list(map(int,input().split())) for _ in range(n)]
 
-for i in range(2, int(sqrt(MAX))+2):
-    if p[i]==0:
-        continue
-    for j in range(i*2,MAX,i):
-        if j>MAX:
-            break
-        p[j] = 0
+a.sort()
 
-n, m =map(int,input().split())
-a = list(map(int,input().split()))+[0]
-d = [[-1]*MAX for _ in range(n+1)]
-
-def dfs(i,c,w):
-    if c>m or i>n:
-        return
-    if d[i][w]!=-1:
-        return
-    if c==m:
-        d[i][w] = 1
-
-    dfs(i+1,c,w)
-    dfs(i+1,c+1,w+a[i])
-dfs(0,0,0)
-res = []
-for i in range(MAX):
-    if d[n][i]==1 and p[i]:
-        res.append(i)
-if res:
-    print(*res)
-else:
-    print(-1)
+res = 0
+l = -maxsize
+r = -maxsize
+for i in range(n):
+    if r<a[i][0]:
+        res += r-l
+        l = a[i][0]
+        r = a[i][1]
+    else:
+        r = max(r,a[i][1])
+res += r-l
+print(res)
