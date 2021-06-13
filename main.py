2f0e2879a514ddbaa740497d6234187cfc5a3da7
Replit user <ha4219@naver.com>
2021-06-13 14:19:54 +0000

    solved boj 21924

--- main.py
+++ main.py
@@ -5,61 +5,61 @@ from collections import deque
 input = stdin.readline
 setrecursionlimit(10**6)
 
-n, m = map(int,input().split())
-a = [list(map(int,input().split())) for _ in range(n)]
+class DisjointSet:
+  def __init__(self, n):
+    self.data = list(range(n))
+    self.size = n
 
-d1 = [[-1]*m for _ in range(n)]
-d2 = [[-1]*m for _ in range(n)]
-dx,dy = (0,-1,0),(1,0,-1)
+  def find(self, index):
+    if self.data[index]==index:
+        return index
+    self.data[index] = self.find(self.data[index])
+    return self.data[index]
 
-def dfs1(sx,sy):
-    q = deque()
-    q.append((sx,sy))
-    d1[sx][sy] = a[sx][sy]
-    while q:
-        x,y=q.popleft()
-        for i in range(2):
-            cx,cy=x+dx[i],y+dy[i]
-            if cx<0 or cy<0 or cx>=n or cy>=m:
-                continue
-            if d1[cx][cy]==-1:
-                d1[cx][cy] = d1[x][y]+a[cx][cy]
-                q.append((cx,cy))
-            else:
-                if d1[cx][cy]<d1[x][y]+a[cx][cy]:
-                    d1[cx][cy] = d1[x][y]+a[cx][cy]
-                    q.append((cx,cy))
+  def union(self, x, y):
+    x, y = self.find(x), self.find(y)
+    if y<x:
+        self.data[y] = self.data[x]
+    else:
+        self.data[x] = self.data[y]
 
-def dfs2(sx,sy):
-    q = deque()
-    q.append((sx,sy))
-    d2[sx][sy] = a[sx][sy]
-    while q:
-        x,y=q.popleft()
-        for i in range(1,3):
-            cx,cy=x+dx[i],y+dy[i]
-            if cx<0 or cy<0 or cx>=n or cy>=m:
-                continue
-            if d2[cx][cy]==-1:
-                d2[cx][cy] = d2[x][y]+a[cx][cy]
-                q.append((cx,cy))
-            else:
-                if d2[cx][cy]<d2[x][y]+a[cx][cy]:
-                    d2[cx][cy] = d2[x][y]+a[cx][cy]
-                    q.append((cx,cy))
+    @property
+    def length(self):
+      return len(set(self.data))
+
+selected = []
+def kruskal(v):
+    res = 0
+    s = DisjointSet(v+1)
+    a.sort(key=lambda x:x[2])
+    for u,v,cost in a:
+        if s.find(u)==s.find(v):
+            continue
+        s.union(u,v)
+        selected.append((u,v))
+        res += cost
+    return ret - res
 
 
-# print()
-dfs1(n-1,0)
-dfs2(n-1,m-1)
-def pp(a):
-    for i in a:
-        print(*i)
-# pp(d1)
-# print()
-# pp(d2)
-res = -maxsize
-for i in range(n):
-    for j in range(m):
-        res = max(res,d1[i][j]+d2[i][j])
-print(res)
+n,m=map(int,input().split())
+a = [list(map(int,input().split())) for _ in range(m)]
+aa = [[] for _ in range(n+1)]
+for i in range(m):
+    aa[a[i][0]].append(a[i][1])
+    aa[a[i][1]].append(a[i][0])
+v = [0] * (n+1)
+def dfs(idx):
+    v[idx] = 1
+    for next in aa[idx]:
+        if v[next]:
+            continue
+        dfs(next)
+dfs(1)
+for i in range(1,n+1):
+    if v[i]==0:
+        print(-1)
+        exit(0)
+ret = 0
+for i in range(m):
+    ret += a[i][2]
+print(kruskal(n))
