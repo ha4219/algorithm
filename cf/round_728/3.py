e742fc219e87586c5d60353eb055b8d0b56c043b
정동하 <ha4219@naver.com>
2021-06-26 01:41:42 +0900

    solved boj 3079

--- cf/round_728/3.py
+++ cf/round_728/3.py
@@ -3,3 +3,8 @@ from sys import stdin, setrecursionlimit, maxsize
 
 input = stdin.readline
 setrecursionlimit(10**6)
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int,input().split()))
+    
