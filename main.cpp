58be32bfea0611696f6d3293f01350c6293e3d35
정동하 <ha4219@naver.com>
2021-06-25 18:07:26 +0900

    solved boj 11557

--- main.cpp
+++ main.cpp
@@ -1,14 +1,78 @@
 #include <bits/stdc++.h>
 
+#define MAX 10001
+
+typedef pair<int, int> PAIR;
+typedef tuple<int, int, int> TUPLE;
 typedef long long ll;
+const int INF = 1 << 30;
 
 using namespace std;
 
 
-int n, m;
+int n, m, p;
 
-int main() {    
-    scanf("%d",&n);
-    printf("111 %d\n", n);
+
+int d[101][MAX];
+
+
+int dijkstra(int start, vector<TUPLE> &a){
+    priority_queue<TUPLE, vector<TUPLE>, greater<TUPLE>> pq;
+    pq.push({0,start,0});
+    d[start][0] = 0;
+    while(!pq.empty()){
+        auto cur = pq.top();
+        pq.pop();
+        int cur_t = get<0>(cur);
+        int cur_x = get<1>(cur);
+        int cur_c = get<2>(cur);
+
+        if(cur_x==n){
+            return cur_t;
+        }
+        if(cur_t>d[cur_x][cur_c]){
+            continue;
+        }
+        for(auto next : a[cur_x]){
+            int next_x = get<0>(next);
+            int next_c = get<1>(next);
+            int next_t = get<2>(next);
+            if(cur_c+next_c>m||d[next_x][cur_c+next_c]<=d[cur_x][cur_c]+next_t){
+                continue;
+            }
+            d[next_x][next_c+cur_c] = d[cur_x][cur_c] + next_t;
+            pq.push({cur_t+next_t,next_x,cur_c+next_c});
+        }
+    }
+
+    return -1;
+}
+
+int solve(){
+    for(int i=0;i<101;i++){
+        a[i].clear();
+    }
+    vector<TUPLE> a[101];
+    scanf("%d %d %d",&n,&m,&p);
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            d[i][j] = INF;
+        }
+    }
+    for(int i=0;i<p;i++){
+        int q,w,e,r;
+        scanf("%d %d %d %d",&q,&w,&e,&r);
+        a[q].push_back({w,e,r});
+    }
+    int res = dijkstra(1, a);
+    return 1;
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    for(int i=0;i<T;i++){
+        solve();
+    }
     return 0;
 }
--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from sys import stdin, setrecursionlimit, maxsize
 input = stdin.readline
 setrecursionlimit(10**6)
 
-n = int(input())
-s = input().strip()
 
-if n<=25:
-    print(s)
-else:
-    t = s[11:-11]
-    tt = t.find('.')
-    if tt==-1 or len(t)-1==tt:
-        print(s[:11]+'...'+s[-11:])
-    else:
-        print(s[:9]+'.'*6+s[-10:])
+res1 = 0
+res2 = ''
+for _ in range(int(input())):
+    for __ in range(int(input())):
+        s, nn = input().split()
+        nn = int(nn)
+        if nn>res1:
+            res1 = nn
+            res2 = s
+    print(res2)
+    
