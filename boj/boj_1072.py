76263c33235639c526883b5bfa1bbc4cbb8f175c
정동하 <ha4219@naver.com>
2021-06-28 03:21:28 +0900

    solved 1072

--- boj/boj_1072.py
+++ boj/boj_1072.py
@@ -10,15 +10,16 @@ setrecursionlimit(10**6)
 
 x, y = map(int, input().split())
 
-if x==y:
-    print(-1)
-    exit(1)
 
 t = (y*100//x)
 l = 0
 r = 1000000000*2
 res = maxsize
 
+if t>=99:
+    print(-1)
+    exit(1)
+    
 while l<=r:
     m = (l+r) // 2
     tt = ((m+y)*100//(m+x))
