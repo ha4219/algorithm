8f3ad64c0eddfeb09950094425a76dc30f1e811b
정동하 <ha4219@naver.com>
2021-07-13 17:16:13 +0900

    [ha4219/ucpc 2020 예선] 20210713 algorithm study solved boj 19538

--- main.py
+++ main.py
@@ -8,58 +8,15 @@ input = stdin.readline
 setrecursionlimit(10**6)
 
 
-def f(s, bit):
-    sl = len(s)
-    # print(sl, s)
-    if sl==1:
-        if s[0]=='0':
-            return 0
-        elif s[0]=='1':
-            return 1
-        else:
-            x = bit&(1<<(ord(s[0])-ord('a')))
-            return x
-    idx = s.find('?')
-    if(idx==-1):
-        if s[0]=='0' or s[0]=='1':
-            x= int(s[0])
-        else:
-            x = 1 if bit&(1<<(ord(s[0])-ord('a'))) else 0
-        if s[3]=='0' or s[3]=='1':
-            y=int(s[3])
-        else:
-            y = 1 if bit&(1<<(ord(s[3])-ord('a'))) else 0
-        if x==y:
-            return 1
-        else:
-            return 0
-    else:
-        eq = s.find(':')
-        if s[0]=='0' or s[0]=='1':
-            x= int(s[0])
-        else:
-            x = 1 if bit&(1<<(ord(s[0])-ord('a'))) else 0
-        if idx==1:
-            if x:
-                return f(s[2:eq], bit)
-            else:
-                return f(s[eq+1:], bit)
-        else:
-            if s[3]=='0' or s[3]=='1':
-                y=int(s[3])
-            else:
-                y = 1 if bit&(1<<(ord(s[3])-ord('a'))) else 0
-            # print(x,y,idx,sl)
-            if x==y:
-                return f(s[5:eq], bit)
-            else:
-                return f(s[eq+1:], bit)
-
 n = int(input())
-s = input().strip()
-MAX = 1<<n
-res = 0
-for i in range(MAX):
-    if f(s, i)==0:
-        res += 1
-print(res)
+a = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    p,q=map(int,input().split())
+    a[p].append(q)
+    a[q].append(p)
+
+d = g = 0
+for i in range(1,n+1):
+    if len(a[i])==3:
+        g += 1
+    
