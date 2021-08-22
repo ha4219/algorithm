320b6edbe2b2092fb8df639ffe2007876812670a
ha4219 <ha4219@naver.com>
2021-08-23 04:26:23 +0900

    solved boj 22966

---
+++ boj/boj_22935.py
@@ -0,0 +1,8 @@
+from sys import stdin, setrecursionlimit, maxsize
+from collections import deque
+from heapq import *
+from math import sqrt, gcd
+
+
+input = stdin.readline
+setrecursionlimit(10**6)
