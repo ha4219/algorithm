160fe2686d293311889901fca9d0fe5d8b96d42e
ha4219 <ha4219@naver.com>
2021-08-24 01:15:59 +0900

    solved boj 20159

--- boj/boj_20159.py
+++ boj/boj_20159.py
@@ -8,22 +8,25 @@ input = stdin.readline
 setrecursionlimit(10**6)
 
 n = int(input())
-a = list(map(int, input().split()))
+a = [0]+list(map(int, input().split()))
 
 res = 0
 odd = [0] * (n+2)
 even = [0] * (n+2)
 
-for i in range(n):
+for i in range(1,n+1):
     if i%2:
-        odd[i+1] = a[i] + odd[i-1]
-        even[i+1] = even[i-1]
+        odd[i] = a[i] + odd[i-1]
+        even[i] = even[i-1]
     else:
-        odd[i+1] = odd[i-1]
-        even[i+1] = a[i] + even[i-1]
+        odd[i] = odd[i-1]
+        even[i] = a[i] + even[i-1]
 
 res = max(odd[n], even[n])
 
 for i in range(1,n):
-    res = max(res, odd[i-1]+even[n-1]-even[i] + a[i-1])
+    if i&1:
+        res = max(res, odd[i-1]+even[n-1]-even[i]+a[n])
+    else:
+        res = max(res, odd[i-1]+even[n-1]-even[i-1])
 print(res)
