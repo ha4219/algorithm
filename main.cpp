685d22663b4d3efe205443b6c1e19c56f66d131e
ha4219 <ha4219@naver.com>
2022-02-10 11:34:25 +0900

    ch

--- main.cpp
+++ main.cpp
@@ -4,73 +4,153 @@
 #define ALL(a) (a).begin(),(a).end()
 #define CASES(t) int aa; cin >> aa; for(int t=1;t<=aa;t++)
 #define MOD 2520
+#define MAX 1000001
 #define INF 9223372036854775807
 typedef long long ll;
 using namespace std;
 
-struct Load {
-    int index;
-    int length;
-    int v_limit;
-};
 
-struct Node {
-	int index;
-	int speed;
-	ll time;
-};
+vector<int> s[MAX*4];
 
-struct compare {
-	bool operator()(const Node& a, const Node& b) {
-		return a.time > b.time;
+void update(int idx, int x, int node, int nL, int nR){
+	if (idx<nL||nR<idx){
+		return;
 	}
-};
+	s[node].push_back(x);
+	if(nL!=nR){
+		int mid = (nL+nR)/2;
+		update(idx, x, node*2, nL, mid);
+		update(idx, x, node*2+1, mid+1, nR);
+	}
+}
 
-int n, m;
-vector<vector<Load>> arr;
+int query(int x, int l, int r, int node, int nL, int nR){
+	if(l>nR||r<nL){
+		return 0;
+	}
+	if(l<=nL&&nR<=r){
+		return upper_bound(s[node].begin(), s[node].end(), x)-s[node].begin();
+	}
+	int mid = (nL+nR)/2;
+	return query(x,l,r,node*2,nL,mid)+query(x,l,r,node*2+1,mid+1,nR);
+}
 
-ll dijkstra() {
-	priority_queue<Node, vector<Node>, compare> pq;
-	pq.push({1, 1, 0});
-	vector<vector<ll>> dist(n + 1, vector<ll>(11, INF));
-	dist[1][1] = 0;
-	while (!pq.empty()) {
-		int here = pq.top().index;
-		int speed = pq.top().speed;
-		ll time = pq.top().time;
-		pq.pop();
-		if (dist[here][speed] < time) continue;
-		for (auto next : arr[here]) {
-			for (int dv = -1; dv <= 1; dv++) {
-				int next_speed = speed + dv;
-				if (next_speed < 1 || next_speed > next.v_limit) continue;
-				ll next_time = time + next.length / next_speed;
-				if (dist[next.index][next_speed] > next_time) {
-					dist[next.index][next_speed] = next_time;
-					pq.push({ next.index, next_speed, next_time });
-				}
+
+string string_add(string a, string b){
+	int sum = 0;
+	string result;
+	while(!a.empty()||!b.empty()||sum){
+		if(!a.empty()) {
+			sum += a.back() -'0';
+			a.pop_back();
+		}
+		if(!b.empty()) {
+			sum += b.back() - '0';
+			b.pop_back();
+		}
+		result.push_back((sum%10) + '0');
+		sum /= 10;
+	}
+	reverse(result.begin(), result.end());
+	return result;
+}
+
+bool isSmaller(string str1, string str2)
+{
+    // Calculate lengths of both string
+    int n1 = str1.length(), n2 = str2.length();
+ 
+    if (n1 < n2)
+        return true;
+    if (n2 < n1)
+        return false;
+ 
+    for (int i = 0; i < n1; i++)
+        if (str1[i] < str2[i])
+            return true;
+        else if (str1[i] > str2[i])
+            return false;
+ 
+    return false;
+}
+
+
+string string_minus(string str1, string str2)
+{
+    // Before proceeding further, make sure str1
+    // is not smaller
+    if (isSmaller(str1, str2))
+        swap(str1, str2);
+ 
+    // Take an empty string for storing result
+    string str = "";
+ 
+    // Calculate length of both string
+    int n1 = str1.length(), n2 = str2.length();
+ 
+    // Reverse both of strings
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+ 
+    int carry = 0;
+ 
+    // Run loop till small string length
+    // and subtract digit of str1 to str2
+    for (int i = 0; i < n2; i++) {
+        // Do school mathematics, compute difference of
+        // current digits
+ 
+        int sub
+            = ((str1[i] - '0') - (str2[i] - '0') - carry);
+ 
+        // If subtraction is less then zero
+        // we add then we add 10 into sub and
+        // take carry as 1 for calculating next step
+        if (sub < 0) {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+ 
+        str.push_back(sub + '0');
+    }
+ 
+    // subtract remaining digits of larger number
+    for (int i = n2; i < n1; i++) {
+        int sub = ((str1[i] - '0') - carry);
+ 
+        // if the sub value is -ve, then make it positive
+        if (sub < 0) {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+ 
+        str.push_back(sub + '0');
+    }
+ 
+    // reverse resultant string
+    reverse(str.begin(), str.end());
+		int idx = -1;
+		for(int i=0;i<str.length();i++){
+			if(str[i]=='0'){
+				idx = i;
+			}else{
+				break;
 			}
 		}
-	}
-	ll ret = INF;
-	for (auto x : dist[n]) ret = min(ret, x);
-	return ret;
+		if(idx==str.length()-1){
+			return "0";
+		}else{
+			return str.substr(idx+1);
+		}
 }
 
 
 int main() {
     FAST;
-    cin >> n >> m;
-    arr = vector<vector<Load>>(n + 1, vector<Load>());
-    for (int i = 0; i < m; i++) {
-        int a, b, l, k;
-        cin >> a >> b >> l >> k;
-        arr[a].push_back({ b,l * MOD,k });
-        arr[b].push_back({ a,l * MOD,k });
-    }
-
-	ll ans = dijkstra();
-    cout<<ans / MOD;
-    string s = to_string((double)(ans % MOD) / MOD);
-	cout<<s<<endl;
+		cout<<string_minus("10000", "5")<<endl;
+		return 0;
 }
