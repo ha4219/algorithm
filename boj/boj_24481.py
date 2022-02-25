74b11159c89075bf628c1864f7154a6c91fb2a32
ha4219 <ha4219@naver.com>
2022-02-25 13:31:23 +0900

    solved boj 24481

---
+++ boj/boj_24481.py
@@ -0,0 +1,33 @@
+from sys import stdin, maxsize, setrecursionlimit
+from heapq import *
+from collections import deque
+import random
+
+MAX = 100001
+MOD = 1000000007
+setrecursionlimit(10**5)
+input = stdin.readline
+
+n, m, r = map(int, input().split())
+a = [[] for _ in range(n+1)]
+
+for _ in range(m):
+  p, q = map(int, input().split())
+  a[p].append(q)
+  a[q].append(p)
+
+v = [0] * (n+1)
+for i in range(1, n+1):
+  a[i].sort()
+
+res = [-1] * (n+1)
+def dfs(cur, dep):
+  v[cur]=1
+  res[cur] = dep
+  for next in a[cur]:
+    if v[next]: continue
+    dfs(next, dep+1)
+  return
+
+dfs(r, 0)
+print('\n'.join(map(str, res[1:])))
