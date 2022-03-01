0c648289a581e4d22afe6d4611b463c5c6e19b0e
ha4219 <ha4219@naver.com>
2022-03-01 11:05:26 +0900

    solved boj 24313

---
+++ boj/boj_24313.py
@@ -0,0 +1,18 @@
+from sys import stdin, maxsize, setrecursionlimit
+from heapq import *
+from collections import deque
+import random
+
+MAX = 100001
+MOD = 1000000007
+setrecursionlimit(10**5)
+input = stdin.readline
+
+a1, a0 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+
+left = a1 * n0 + a0
+right = c * n0
+
+print(1 if left <= right and a1 <= c  else 0)
