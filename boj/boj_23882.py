f78a1233256f5fcafd266622d1d52bed7d8eac50
ha4219 <ha4219@naver.com>
2022-03-01 11:11:13 +0900

    solved boj 24314

---
+++ boj/boj_23882.py
@@ -0,0 +1,26 @@
+from sys import stdin, maxsize, setrecursionlimit
+from heapq import *
+from collections import deque
+import random
+
+MAX = 100001
+MOD = 1000000007
+setrecursionlimit(10**5)
+input = stdin.readline
+
+def solve():
+  n, k = map(int, input().split())
+  a = list(map(int, input().split()))
+
+  cnt = 0
+  for i in range(n):
+    idx = a[:n-i].index(max(a[:n-i]))
+    if idx == n-i-1: continue
+    a[idx], a[n-i-1] = a[n-i-1], a[idx]
+    cnt += 1
+    if cnt == k:
+      print(*a)
+      return
+  print(-1)
+
+solve()
