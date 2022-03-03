7db7fdf042db1150f6572aff19ca0799815c67b6
ha4219 <ha4219@naver.com>
2022-03-03 15:19:01 +0900

    solved boj 24398

---
+++ boj/boj_24398.py
@@ -0,0 +1,53 @@
+from sys import stdin, maxsize, setrecursionlimit
+from heapq import *
+from collections import deque
+import random
+
+MAX = 31623
+MOD = 1000000007
+setrecursionlimit(10**5)
+input = stdin.readline
+
+n, q, K = map(int, input().split())
+a = list(map(int, input().split()))
+
+cnt = 0
+def select(p, r, q):
+  if p==r:
+    return a[p]
+  t = partition(p, r)
+  k = t - p + 1
+  if q < k:
+    return select(p, t-1, q)
+  elif q == k:
+    return a[t]
+  return select(t+1,r,q-k)
+
+def partition(p, r):
+  global cnt
+  x = a[r]
+  i = p - 1
+  for j in range(p, r):
+    if a[j] <= x:
+      i += 1
+      a[i], a[j] = a[j], a[i]
+      cnt += 1
+      if cnt == K:
+        if a[i] < a[j]:
+          print(a[i], a[j])
+        else:
+          print(a[j], a[i])
+        exit(0)
+  if i + 1 != r:
+    a[i+1], a[r] = a[r], a[i+1]
+    cnt += 1
+    if cnt == K:
+      if a[i+1] < a[r]:
+        print(a[i+1], a[r])
+      else:
+        print(a[r], a[i+1])
+      exit(0)
+  return i+1
+select(0, n-1, q)
+
+print(-1)
