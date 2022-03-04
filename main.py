ebc39919e509f378d62e3a0bd782401c6459e6a1
ha4219 <ha4219@naver.com>
2022-03-04 12:04:46 +0900

    check

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ from heapq import *
 from collections import deque
 import random
 
-MAX = 100001
+MAX = 27
 MOD = 1000000007
 setrecursionlimit(10**5)
 input = stdin.readline
 
-a1, a0 = map(int, input().split())
-c = int(input())
-n0 = int(input())
+n = int(input())
+a = []
+for _ in range(n):
+    p, q = map(int, input().split())
+    if p < q:
+        a.append([p, q])
+    else:
+        a.append([q, p])
+a.sort(key=lambda x: x[1])
+d = int(input())
 
-left = a1 * n0 + a0
-right = c * n0
-
-print(1 if left >= right and a1 >= c  else 0)
+pq = []
+res = 0
+for item in a:
+    if item[1] - item[0] > d:
+        continue
+    while pq and item[1] - d > pq[0][0]:
+        heappop(pq)
+    heappush(pq, item)
+    res = max(res, len(pq))
+print(res)
