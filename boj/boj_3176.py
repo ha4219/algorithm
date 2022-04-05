deef11d5a2f5bbfe6af8d2fdba4c7681e033d4dd
ha4219 <ha4219@naver.com>
2022-04-05 16:41:24 +0900

    solved boj 3176

--- boj/boj_3176.py
+++ boj/boj_3176.py
@@ -8,81 +8,57 @@ MOD = 1000000007
 setrecursionlimit(10**5)
 input = stdin.readline
 
-n = int(input())
+for ____ in range(int(input())):
+    n = int(input())
 
-a = [[] for _ in range(n+1)]
-# parent = [[0] * (MAX+1) for _ in range(n+1)]
-parent = [[[0, maxsize, -maxsize] for __ in range(MAX+1)] for _ in range(n+1)]
-d = [0] * (n+1)
+    a = [[] for _ in range(n+1)]
+    v = [0] * (n+1)
 
-for _ in range(n-1):
-    p, q, c = map(int, input().split())
-    a[p].append((q, c))
-    a[q].append((p, c))
+    parent = [[[0, maxsize, -maxsize]
+               for __ in range(MAX+1)] for _ in range(n+1)]
+    d = [0] * (n+1)
 
+    for _ in range(n-1):
+        p, q = map(int, input().split())
+        v[q] = 1
+        a[p].append(q)
 
-def dfs(cur, depth):  # 변화 없음
-    for next, next_cost in a[cur]:
-        if d[next]:
-            continue
-        parent[next][0][0] = cur
-        d[next] = depth + 1
+    p = 0
+    for i in range(1, n+1):
+        if v[i] == 0:
+            p = i
+            break
 
-        parent[next][0][1] = next_cost
-        parent[next][0][2] = next_cost
-        dfs(next, depth+1)
-    return
+    def dfs(cur, depth):
+        for next in a[cur]:
+            if d[next]:
+                continue
+            parent[next][0][0] = cur
+            d[next] = depth + 1
+            dfs(next, depth+1)
+        return
 
+    d[p] = 0
+    dfs(p, 0)
 
-# parent[1][0][0] = 0
-d[1] = 0
-dfs(1, 0)
+    for i in range(1, MAX):
+        for j in range(1, n+1):
+            parent[j][i][0] = parent[parent[j][i-1][0]][i-1][0]
 
-# for i in range(1, n+1):
-#     print(parent[i][0])
+    def solve(l, r):
+        if d[l] > d[r]:
+            l, r = r, l
+        for i in range(MAX, -1, -1):
+            if d[r] - d[l] >= (1 << i):
+                left = parent[r][i][1]
+                r = parent[r][i][0]
+        if l == r:
+            return l
+        for i in range(MAX, -1, -1):
+            if parent[l][i][0] != parent[r][i][0]:
+                l = parent[l][i][0]
+                r = parent[r][i][0]
+        return parent[l][0][0]
 
-# parent[node][2^i 번째 parent 값]
-for i in range(1, MAX):
-    for j in range(1, n+1):
-        # parent[j][i] = parent[parent[j][i-1]][i-1]
-        parent[j][i][0] = parent[parent[j][i-1][0]][i-1][0]
-        parent[j][i][1] = min(
-            parent[j][i-1][1], parent[parent[j][i-1][0]][i-1][1])
-        parent[j][i][2] = max(
-            parent[j][i-1][2], parent[parent[j][i-1][0]][i-1][2])
-        # print(i, parent[j][i][0], parent[j][i-1][0], j, parent[j][i][1:])
-
-cache = {}
-
-
-def solve(l, r):
-    left, right = maxsize, -maxsize
-    if d[l] > d[r]:
-        l, r = r, l
-    for i in range(MAX, -1, -1):
-        if d[r] - d[l] >= (1 << i):
-            left = min(left, parent[r][i][1])
-            right = max(right, parent[r][i][2])
-            r = parent[r][i][0]
-    if l == r:
-        return (left, right)
-    for i in range(MAX, -1, -1):
-        if parent[l][i][0] != parent[r][i][0]:
-            left = min(left, parent[l][i][1], parent[r][i][1])
-            right = max(right, parent[l][i][2], parent[r][i][2])
-
-            l = parent[l][i][0]
-            r = parent[r][i][0]
-    left = min(left, parent[l][i][1], parent[r][i][1])
-    right = max(right, parent[l][i][2], parent[r][i][2])
-    return (left, right)
-
-
-m = int(input())
-for _ in range(m):
     p, q = map(int, input().split())
-    print(*solve(p, q))
-
-
-# for i in parent:
-#     print(i[:5])
+    print(solve(p, q))
