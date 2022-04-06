5523575dc8cf91106e49c80bc95d6cc6c0557486
ha4219 <ha4219@naver.com>
2022-04-06 22:57:09 +0900

    solved boj 9345

--- main.py
+++ main.py
@@ -8,61 +8,70 @@ MOD = 1000000007
 setrecursionlimit(10**5)
 input = stdin.readline
 
-n = int(input())
 
-a = [[] for _ in range(n+1)]
-parent = [[0] * (MAX+1) for _ in range(n+1)]
-d = [0] * (n+1)
-dis = [0] * (n+1)
-v = [0] * (n+1)
-
-for _ in range(n-1):
-    p, q, c = map(int, input().split())
-    a[p].append((q, c))
-    a[q].append((p, c))
-
-
-def dfs(cur, depth, distance):
-    if v[cur]:
+# nlogn??
+def u(i, v, node, nL, nR):
+    # if nL == nR:
+    #     s[node] = v
+    #     return s[node]
+    if nL > i or i > nR:
         return
-
-    for next, next_cost in a[cur]:
-        if d[next] != 0:
-            continue
-        parent[next][0] = cur
-        dis[next] = distance + next_cost
-        d[next] = depth+1
-        dfs(next, depth+1, dis[next])
+    if nL == nR:
+        s_min[node] = v
+        s_max[node] = v
+        return
+    m = (nL+nR)//2
+    u(i, v, node*2, nL, m)
+    u(i, v, node*2+1, m+1, nR)
+    s_min[node] = min(s_min[node*2], s_min[node*2+1])
+    s_max[node] = max(s_max[node*2], s_max[node*2+1])
     return
 
 
-dfs(1, 1, 0)
+def q_min(l, r, node, nL, nR):
+    if nL > r or nR < l:
+        return maxsize
+    if l <= nL and nR <= r:
+        return s_min[node]
+    m = (nL+nR)//2
+    return min(q_min(l, r, node*2, nL, m), q_min(l, r, node*2+1, m+1, nR))
 
 
-for i in range(1, MAX):
-    for j in range(1, n+1):
-        parent[j][i] = parent[parent[j][i-1]][i-1]
-
-cache = {}
-
-
-def solve(l, r):
-    if d[l] > d[r]:
-        l, r = r, l
-    for i in range(MAX, -1, -1):
-        if d[r] - d[l] >= (1 << i):
-            r = parent[r][i]
-    if l == r:
-        return l
-    for i in range(MAX, -1, -1):
-        if parent[l][i] != parent[r][i]:
-            l = parent[l][i]
-            r = parent[r][i]
-    return parent[l][0]
+def q_max(l, r, node, nL, nR):
+    if nL > r or nR < l:
+        return -maxsize
+    if l <= nL and nR <= r:
+        return s_max[node]
+    m = (nL+nR)//2
+    return max(q_max(l, r, node*2, nL, m), q_max(l, r, node*2+1, m+1, nR))
 
 
-m = int(input())
-for _ in range(m):
-    p, q = map(int, input().split())
-    ancestor = solve(p, q)
-    print(dis[p] + dis[q] - dis[ancestor] * 2)
+for ____ in range(int(input())):
+    n, k = map(int, input().split())
+
+    arr = [i for i in range(n)]
+    s_min = [maxsize] * (4*n)
+    s_max = [maxsize] * (4*n)
+
+    '''
+    min
+    max
+    '''
+
+    for i in range(n):
+        u(i, i, 1, 0, n-1)
+
+    for _ in range(k):
+        a, b, c = map(int, input().split())
+
+        if a:
+            l = q_min(b, c, 1, 0, n-1)
+            r = q_max(b, c, 1, 0, n-1)
+
+            print('YES' if l == b and r == c else 'NO')
+        else:
+            left = arr[b]
+            right = arr[c]
+            u(b, right, 1, 0, n-1)
+            u(c, left, 1, 0, n-1)
+            arr[b], arr[c] = arr[c], arr[b]
