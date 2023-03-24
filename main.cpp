901b3fe4472568104a484325af6970f68b824b5d
ha4219 <ha4219@naver.com>
2023-03-24 13:50:12 +0900

    update all

--- main.cpp
+++ main.cpp
@@ -41,71 +41,13 @@ using namespace std;
 
 int n;
 
-PLL get_max(int t) {
-    if (t == 0) return {0,0};
-    ll day = t - (t / 7) * 2;
-
-    if (t - (t/7)*7 == 6) {
-        day--;
-    }
-
-    return {day, t};
-}
-
-int solve() {
-    cin>>n;
-    vector<PII> a(n);
-    
-    for (int i=0;i<n;i++) {
-        int l, r;
-        cin>>l>>r;
-
-        a[i] = {l, r};
-    }
-
-    sort(a.begin(), a.end());
-
-    int t = a[0].se;
-    PLL prev = get_max(a[0].fi);
-    if (t > prev.fi + prev.se) {
-        cout<<"-1\n";
-        return 0;
-    }
-    int res = a[0].se <= prev.fi ? 0 : a[0].se - prev.fi;
-    int residual = a[0].se <= prev.fi ? prev.fi - a[0].se : 0;
-    
-    for(int i=1;i<n;i++) {
-        t += a[i].se;
-        PLL current = get_max(a[i].fi);
-        if (t > current.fi + current.se) {
-            cout<<"-1\n";
-            return 0;
-        }
-
-        PLL dif = {current.fi - prev.fi, current.se - prev.se};
-
-        if (a[i].se <= dif.fi) { // 그냥 가능
-            residual += dif.fi - a[i].se;
-        } else if (a[i].se <= dif.fi + residual) { // residual 써야 가능
-            residual -= a[i].se - dif.fi;
-        } else { // residual 써도 불가능
-            res += a[i].se - dif.fi - residual;
-            residual = 0;
-        }
-
-        prev = current;
-    }
-    cout<<res<<'\n';
-    return 0;
-}
-
 int main() {
     FAST;
-    int t = 1;
     // cin>>t;
-    TC(t){
-        solve();
-    }
-    
+    // TC(t){
+    //     solve();
+    // }
+    cout<<numeric_limits<float>::max()+numeric_limits<float>::max()<<'\n';
+        
     return 0;
 }
--- main.py
+++ main.py
@@ -11,10 +11,37 @@ MOD = 1000000007
 setrecursionlimit(10**5)
 input = stdin.readline
 
-n = int(input())
 
-a = list(map(int, input().split()))
+N = int(input())
+A = [list(map(int, input().split())) for _ in range(N)]
+lr = [0 for _ in range(N*2-1)]
+rl = [0 for _ in range(N*2-1)]
+
+get_lr_index = lambda x, y: x - y + N - 1
+get_rl_index = lambda x, y: x + y  
+a_odd = [(i, j) for i in range(N) for j in range(N) if A[i][j] and (i+j) % 2]
+a_even = [(i, j) for i in range(N) for j in range(N) if A[i][j] and (i+j) % 2 == 0]
+
 res = 0
-for v in a:
-    res = 1 - (1-res)*(1-v/100)
-    print(res * 100)
+
+# a = 100, lr = 19, 100 ^ 19? => TLE?
+# a = 100, next_index, with type
+def dfs(next_index, d, t):
+    res = d
+    a = a_odd if t else a_even
+    for i, (x, y) in enumerate(a[next_index:], next_index):
+        lr_i, rl_i = get_lr_index(x, y), get_rl_index(x, y)
+        if lr[lr_i] == rl[rl_i] == 0:
+            lr[lr_i] = 1
+            rl[rl_i] = 1
+            res = max(dfs(i+1, d+1, t), res)
+            lr[lr_i] = 0
+            rl[rl_i] = 0
+    return res
+
+
+def main():
+    print(dfs(0, 0, 0) + dfs(0, 0, 1))
+
+if __name__ == '__main__':
+    main()
