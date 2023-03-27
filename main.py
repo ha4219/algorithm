55ca68c440b5b97652e3c50be4f48e5e84bea05b
ha4219 <ha4219@naver.com>
2023-03-27 16:09:34 +0900

    update

--- main.py
+++ main.py
@@ -8,16 +8,50 @@ from itertools import combinations
 
 MAX = 17
 MOD = 1000000007
-setrecursionlimit(10**5)
+setrecursionlimit(10**6)
 input = stdin.readline
 
 
-N, K, Q = map(int, input().split())
-a = list(map(int, input().split()))
-xs = list(map(int, input().split()))
+N = int(input())
+a =[[] for _ in range(N)]
+
+for _ in range(N-1):
+    l, r = map(int, input().split())
+    a[l-1].append(r-1)
+    a[r-1].append(l-1)
+
+leaf = set()
+d1 = [maxsize] * N
+d2 = [maxsize] * N
+d3 = [maxsize] * N
+
+qs = list(map(int, input().split()))
+
+def dfs(cur, depth, d):
+    if len(a[cur]) == 1:
+        leaf.add(cur)
+    for nn in a[cur]:
+        if d[nn] > depth+1:
+            d[nn] = depth+1
+            dfs(nn, depth+1, d)
+
+d1[qs[0]-1] = 0
+dfs(qs[0]-1, 0, d1)
+
+d2[qs[1]-1] = 0
+dfs(qs[1]-1, 0, d2)
+
+d3[qs[2]-1] = 0
+dfs(qs[2]-1, 0, d3)
 
 def main():
-    print(a)
+    for i in leaf:
+        if d1[i] < d2[i] and d1[i] <d3[i]:
+            print('YES')
+            return
+    print('NO')
+    return
+    
 
 
 if __name__ == '__main__':
