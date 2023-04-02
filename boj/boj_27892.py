b5bf40eb292cc9aaabad1e16a1a2c041a8d23865
ha4219 <ha4219@naver.com>
2023-04-02 16:36:52 +0900

    solved boj 27892

--- boj/boj_27892.py
+++ boj/boj_27892.py
@@ -11,43 +11,29 @@ MOD = 1000000007
 setrecursionlimit(10**6)
 input = stdin.readline
 
-x0, N = map(int, input().split())
-d = {}
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
 
-item = x0
-i = 0
-
-while item is not None:
-    next_value = (item * 2) ^ 6 if (item & 1) else (item // 2) ^ 6
-    new = d.get(next_value)
-    if new:
-        d[item] = (next_value, i)
-        cycle = ((next_value, new[1]), i - new[1] + 1)
-        break
-    d[item] = (next_value, i)
-    item = next_value
-    i += 1
-# print(d)
-# print(d[7])
-# print(cycle)
 
 def main():
-    cycle_value, cycle_start_index, cycle_cnt = cycle[0][0], cycle[0][1], cycle[1]
+    s = [0] * N
+    s[0] = a[0]
 
-    if N <= cycle_start_index:
-        x = x0
-        for _ in range(N):
-            x = d[x][0]
-        print(x)
-    else:
-        n = N
-        n -= cycle_start_index
-        n %= cycle_cnt
-        x = cycle_value
-        # print(x, n)
-        for _ in range(n):
-            x = d[x][0]
-        print(x)
+    pq = [-a[0]]
+    cnt = 0
+
+    if s[0] >= M:
+        s[0] += 2 * heappop(pq)
+        cnt += 1
+
+    for i in range(1, N):
+        s[i] = s[i-1] + a[i]
+        heappush(pq, -a[i])
+        while pq and  s[i] >= M:
+            x = heappop(pq)
+            s[i] += x * 2
+            cnt += 1
+    print(cnt)
     return
 
 if __name__ == '__main__':
