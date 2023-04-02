5f8bfd202925c79d628a41137440a3e60b4e6b74
ha4219 <ha4219@naver.com>
2023-04-02 16:38:26 +0900

    solved boj 27896

--- boj/boj_27892.py
+++ boj/boj_27892.py
@@ -12,28 +12,9 @@ setrecursionlimit(10**6)
 input = stdin.readline
 
 N, M = map(int, input().split())
-a = list(map(int, input().split()))
-
+s = input().strip()
 
 def main():
-    s = [0] * N
-    s[0] = a[0]
-
-    pq = [-a[0]]
-    cnt = 0
-
-    if s[0] >= M:
-        s[0] += 2 * heappop(pq)
-        cnt += 1
-
-    for i in range(1, N):
-        s[i] = s[i-1] + a[i]
-        heappush(pq, -a[i])
-        while pq and  s[i] >= M:
-            x = heappop(pq)
-            s[i] += x * 2
-            cnt += 1
-    print(cnt)
     return
 
 if __name__ == '__main__':
