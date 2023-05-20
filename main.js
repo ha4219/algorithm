b30c21e0e23ff52c31948273201293d84f7a5e0b
ha4219 <ha4219@naver.com>
2023-05-20 17:49:11 +0900

    solved boj 28014

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
-
+text = '콰트로 맥시멈 미트 포커스드 어메이징 얼티밋 그릴드 패티 오브 더 비기스트 포 슈퍼 미트 프릭'
+input = document.getElementById('input_typing');
+input.focus();
+start_typing();
+for(const v in text){
+    input.value = input.value + text.charAt(v);
+}setTimeout(()=>{stop_typing()},1)
